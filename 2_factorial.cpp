51e11dda39b1c466a38ec70544234467e70a1b7c
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-07-23 15:45:53 +0530

    Two factorial numbers, 1st is for 1 to 12 and second for numbers greater than 12

--- 2_factorial.cpp
+++ 2_factorial.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// creating a factorial fucntion
+// creating a factorial fucntion for numbers upto 1 to 12 
 int factorial(int n) {
     int fact=1;
     for (int i=1; i<=n;i++){
@@ -10,7 +10,17 @@ int factorial(int n) {
     return fact;
 }
 
+// Function for factorial greater than 12
+double d_factorial(int n) {
+    double fact=1;
+    for (double i=1; i<=n;i++){
+        fact*=i;
+    }
+    return fact;
+}
+
 int main() {
-    cout << factorial(5)<<endl;
+    cout << factorial(12)<<endl;
+    cout << d_factorial(15) << endl;
     return 0;
 }
